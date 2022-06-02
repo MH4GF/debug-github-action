52db7f697b04686bbf2ac6fe5190fdc8d9d77939
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-03 08:39:34 +0900

    test setFailed

--- src/script.js
+++ src/script.js
@@ -1,9 +1,3 @@
 module.exports = async ({ github, context, core }) => {
-  const { SHA } = process.env;
-  const commit = await github.rest.repos.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `${SHA}`,
-  });
-  core.exportVariable("author", commit.data.commit.author.email);
+  core.setFailed("test error message")
 };
