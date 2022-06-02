f07e122def54c4a67bfcdffc7df0f1acf15ee739
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-03 08:44:20 +0900

    debug unhandled promise

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 module.exports = async ({ github, context, core }) => {
-  core.setFailed("test error message")
+  try {
+    new Promise(function (resolve, reject) {
+      throw new Error("Whoops!");
+    });
+  } catch (error) {
+    console.log({ error });
+    if (error instanceof Error) core.setFailed(error);
+  }
 };
