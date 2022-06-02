ccebb2655c81a308ca7ad09d99da4b03f62610a2
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-03 08:53:12 +0900

    unwrap try-catch

--- src/script.js
+++ src/script.js
@@ -1,12 +1,7 @@
 module.exports = async ({ github, context, core }) => {
-  try {
-    new Promise(function (resolve, reject) {
-      throw new Error("Whoops!");
-    }).catch((e) => {
-      throw e;
-    });
-  } catch (error) {
-    console.log({ error });
-    // if (error instanceof Error) core.setFailed(error);
-  }
+  new Promise(function (resolve, reject) {
+    throw new Error("Whoops!");
+  }).catch((e) => {
+    throw e;
+  });
 };
