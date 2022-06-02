d295799ebbe1088527bf6cb858110e1ce6242c7b
MH4GF <h.miyagi.cnw@gmail.com>
2022-06-03 08:51:21 +0900

    catch promise

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ module.exports = async ({ github, context, core }) => {
   try {
     new Promise(function (resolve, reject) {
       throw new Error("Whoops!");
+    }).catch((e) => {
+      throw e;
     });
   } catch (error) {
     console.log({ error });
